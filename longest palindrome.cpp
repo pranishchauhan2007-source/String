ff2cb47031de5c7821b22d1f666277a269e9e578
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-09-16 02:41:45 +0530

    Longest palindrome

---
+++ longest palindrome.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    int count = 0;
+    bool odd = 0;
+
+    vector<int> lower(26, 0), upper(26,0);
+
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i]>='a')
+        lower[s[i] - 'a']++;
+        else
+        upper[s[i] - 'A']++;
+    }
+
+    for(int i = 0; i < 26; i++)
+    {
+        if(lower[i]%2 == 0)
+        {
+            count += lower[i];
+        }
+        else
+        {
+            count += lower[i]-1;
+            odd=1;
+        }
+        if(upper[i]%2 == 0)
+        {
+           count += upper[i];
+        }
+        else
+        {
+           count += upper[i]-1;
+           odd=1;
+        }
+    }
+
+    count += odd;
+    cout<<count;
+    return 0;
+}

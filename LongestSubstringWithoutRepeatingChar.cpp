3f425000da57a2b1b4bab6e59d78801a6b980fdb
pranishchauhan2007-source <pranishchauhan2007@gmail.com>
2026-09-21 00:53:43 +0530

    Longest substring without repeating char

---
+++ LongestSubstringWithoutRepeatingChar.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    vector<bool>count(256,0);
+    int first = 0, second = 0, len = 0;
+    while (second < s.size())
+    {
+     while (count [s[second]])
+     {
+      count[s[first]] = 0;
+      first++;
+     }
+     count[s[second]] = 1;
+     len = max(len, second-first+1);
+     second++;
+    }
+    cout<<len;
+}
